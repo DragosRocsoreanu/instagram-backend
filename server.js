daf70bacd07fc51fcbdc8cc65dee3f586b166eb0
DragosRocsoreanu <rocsoreanu.dragos@gmail.com>
2019-03-23 18:45:04 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,50 @@
+// Import Dependencies
+const { ApolloServer, AuthenticationError } = require('apollo-server');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+
+// Import typeDefs and resolvers
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const resolvers = require('./resolvers')
+
+// Import Environment Variables and Mongoose Models
+require('dotenv').config({ path: 'variables.env' });
+const User = require('./models/User');
+const Post = require('./models/Post');
+
+// Connect to MLab Database
+mongoose
+	.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(() => console.log("Database Connected"))
+	.catch(err => console.error(err));
+
+mongoose.set('useCreateIndex', true);
+
+// verify JWT Token passed from client
+const getUser = async token => {
+	if (token) {
+		try {
+			return await jwt.verify(token, process.env.SECRET);
+		} catch (err) {
+			throw new AuthenticationError('Your session has ended. Please sign in again.')
+		}
+	}
+}
+
+// Create Apollo/GraphQL Server using typeDefs, resolvers and context object
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: async ({ req }) => {
+		const token = req.headers['authorization'];
+		return { User, Post, currentUser: await getUser(token) };
+	}
+});
+
+// Start server listen
+server.listen(4000).then(({ url }) => {
+	console.log(`Server ready at ${url}`);
+});
